ff136c0b3dbd0b7f52bae0a81edb088fc18f7aad
reshinto <terencekong2002@gmail.com>
2021-10-23 16:34:43 +0800

    Update decorator pattern

--- Structural_patterns/Decorator/python/Decorator.py
+++ Structural_patterns/Decorator/python/Decorator.py
@@ -1,46 +1,27 @@
-class WindowInterface:
-    def build(self): pass
+from Component import Component
 
 
-class AbstractWindowDecorator(WindowInterface):
+class Decorator(Component):
     """
-    Maintains a reference to a window object and define an interface
-    that conforms to a window's interface.
+    The base Decorator class follows the same interface as the other components.
+    The primary purpose of this class is to define the wrapping interface for
+    all concrete decorators. The default implementation of the wrapping code
+    might include a field for storing a wrapped component and the means to
+    initialize it.
     """
 
-    def __init__(self, window):
-        self._window = window
+    _component: Component = None
 
-    def build(self): pass
+    def __init__(self, component: Component) -> None:
+        self._component = component
 
+    @property
+    def component(self) -> str:
+        """
+        The Decorator delegates all work to the wrapped component.
+        """
 
-class Window(WindowInterface):
-    def build(self):
-        print("Building window")
+        return self._component
 
-
-class BorderDecorator(AbstractWindowDecorator):
-    def add_border(self):
-        print("Adding border")
-
-    def build(self):
-        self.add_border()
-        self._window.build()
-
-
-class VerticalSBDecorator(AbstractWindowDecorator):
-    def add_vertical_scroll_bar(self):
-        print("Adding vertical scroll bar")
-
-    def build(self):
-        self.add_vertical_scroll_bar()
-        self._window.build()
-
-
-class HorizontalSBDecorator(AbstractWindowDecorator):
-    def add_horizontal_scroll_bar(self):
-        print("Adding horizontal scroll bar")
-
-    def build(self):
-        self.add_horizontal_scroll_bar()
-        self._window.build()
+    def operation(self) -> str:
+        return self._component.operation()
--- Structural_patterns/Decorator/python/run.py
+++ Structural_patterns/Decorator/python/run.py
@@ -1,17 +1,43 @@
-from Decorator import Window, BorderDecorator, VerticalSBDecorator, HorizontalSBDecorator
+"""
+Decorator Design Pattern
 
-# Build a window
-w = Window()
-w.build()
+Intent: Lets you attach new behaviors to objects by placing these objects inside
+special wrapper objects that contain the behaviors.
+"""
 
-# Add a border to the window before building a window
-wb = BorderDecorator(w)
-wb.build()
 
-# Add a vertical scroll, then border before building a window
-wbv = VerticalSBDecorator(wb)
-wbv.build()
+from Component import Component
+from ConcreteComponent import ConcreteComponent
+from ConcreteDecoratorA import ConcreteDecoratorA
+from ConcreteDecoratorB import ConcreteDecoratorB
 
-# Add a horizontal scroll, then vertical scroll, then border before building a window
-best_window = HorizontalSBDecorator(wbv)
-best_window.build()
+
+def client_code(component: Component) -> None:
+    """
+    The client code works with all objects using the Component interface. This
+    way it can stay independent of the concrete classes of components it works
+    with.
+    """
+
+    # ...
+
+    print(f"RESULT: {component.operation()}", end="")
+
+    # ...
+
+
+if __name__ == "__main__":
+    # This way the client code can support both simple components...
+    simple = ConcreteComponent()
+    print("Client: I've got a simple component:")
+    client_code(simple)
+    print("\n")
+
+    # ...as well as decorated ones.
+    #
+    # Note how decorators can wrap not only simple components but the other
+    # decorators as well.
+    decorator1 = ConcreteDecoratorA(simple)
+    decorator2 = ConcreteDecoratorB(decorator1)
+    print("Client: Now I've got a decorated component:")
+    client_code(decorator2)
